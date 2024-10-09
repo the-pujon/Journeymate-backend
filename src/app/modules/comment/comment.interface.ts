5235c647fb5274167b0db0372679959e02744aa7
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-09 10:13:47 +0600

    comment model and interface added

--- src/app/modules/comment/comment.interface.ts
+++ src/app/modules/comment/comment.interface.ts
@@ -1,1 +1,10 @@
- 
+import { Types } from "mongoose";
+
+export interface TComment {
+  author: Types.ObjectId;
+  content: string;
+  post: Types.ObjectId;
+  upVotes?: number;
+  downVotes?: number;
+  user?: Types.ObjectId;
+}
--- src/app/modules/comment/comment.model.ts
+++ src/app/modules/comment/comment.model.ts
@@ -1,1 +1,41 @@
- 
+import { Schema, model } from "mongoose";
+import { TComment } from "./comment.interface";
+
+const commentSchema = new Schema<TComment>(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    upVotes: {
+      type: Number,
+      default: 0,
+    },
+    downVotes: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: undefined,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Comment = model<TComment>("Comment", commentSchema);
+
+export default Comment;
