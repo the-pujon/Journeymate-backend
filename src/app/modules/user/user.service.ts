9867bb575b03fb9b8eeda41ed634812116286add
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-09 13:52:14 +0600

    get user working with search. used aggregationPipeline

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,51 +1,53 @@
-import { FilterQuery } from "mongoose";
 import UserProfile from "./user.model";
 import { TUserProfile } from "./user.interface";
 
-// ... existing code ...
-
 const getUsers = async (searchQuery?: string): Promise<TUserProfile[]> => {
-  const filter: FilterQuery<TUserProfile> = {};
+  const aggregationPipeline = [];
+
+  aggregationPipeline.push({
+    $lookup: {
+      from: "users",
+      localField: "user",
+      foreignField: "_id",
+      as: "userDetails",
+    },
+  });
+
+  aggregationPipeline.push({ $unwind: "$userDetails" });
 
   if (searchQuery) {
-    filter["$or"] = [
-      { "user.name": { $regex: searchQuery, $options: "i" } },
-      //  { 'user.username': { $regex: searchQuery, $options: 'i' } },
-    ];
-  }
-
-  const users = await UserProfile.find(filter)
-    .populate({
-      path: "user",
-      select: "name username email",
-    })
-    .populate({
-      path: "followers",
-      populate: {
-        path: "user",
-        select: "name username email",
-      },
-    })
-    .populate({
-      path: "following",
-      populate: {
-        path: "user",
-        select: "name username email",
-      },
-    })
-    .populate({
-      path: "posts",
-      select: "title content createdAt",
-      populate: {
-        path: "author",
-        select: "name username",
+    aggregationPipeline.push({
+      $match: {
+        $or: [
+          { "userDetails.name": { $regex: searchQuery, $options: "i" } },
+          { "userDetails.email": { $regex: searchQuery, $options: "i" } },
+        ],
       },
     });
+  }
+
+  aggregationPipeline.push({
+    $project: {
+      _id: 1,
+      user: {
+        _id: "$userDetails._id",
+        name: "$userDetails.name",
+        email: "$userDetails.email",
+      },
+      verified: 1,
+      followers: 1,
+      following: 1,
+      posts: 1,
+      createdAt: 1,
+      updatedAt: 1,
+    },
+  });
+
+  const users = await UserProfile.aggregate(aggregationPipeline);
 
   return users;
 };
 
 export const UserService = {
-  // ... existing methods ...
   getUsers,
 };
