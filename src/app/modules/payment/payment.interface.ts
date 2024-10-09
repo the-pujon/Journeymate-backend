63208db1b217c4d3df37207da92a61c6461db5a9
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-09 10:15:09 +0600

    payment model and interface added

--- src/app/modules/payment/payment.interface.ts
+++ src/app/modules/payment/payment.interface.ts
@@ -1,1 +1,8 @@
- 
+import { Types } from "mongoose";
+
+export interface TPayment {
+  user: Types.ObjectId;
+  amount: number;
+  status: "pending" | "success" | "failed";
+  transactionId: string;
+}
--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -1,1 +1,32 @@
- 
+import { Schema, model } from "mongoose";
+import { TPayment } from "./payment.interface";
+
+const paymentSchema = new Schema<TPayment>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "success", "failed"],
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Payment = model<TPayment>("Payment", paymentSchema);
+
+export default Payment;
